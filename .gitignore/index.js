cb07d759d4a86a3ed2d66c1c83376399bd16b8b0
AntoinePontiac <36533390+AntoinePontiac@users.noreply.github.com>
2018-02-16 15:40:47 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+comst discord = require ("discord.j");
+
+var bot = new discord.clien();
+
+bot.on "ready", fonction[] {
+    bot.user.setgame("ZeldaBot, !help);
+    console.log ("le bot est bien lance");
+});
+
+bot.login("NDE0MDU4NzE3NTMzODMxMTY5.DWh1yA.BfK_G8HUqZHpFKaeMsVTayumzCk");
